22c91d238ed15b08a767f032bec5573ecffb8a99
rodriguezdiego326-max <rodriguezdiego326@gmail.com>
2026-09-06 22:58:42 -0500

    Updated index.html

---
+++ ios/App/App/public/assets/index-BbsFyH7f.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-DHMHpeBH.js","./index-CWIuK-0e.js","./index-CUeZ3PqT.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-CWIuK-0e.js";const _=r("SplashScreen",{web:()=>t(()=>import("./web-DHMHpeBH.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.SplashScreenWeb)});export{_ as SplashScreen};
